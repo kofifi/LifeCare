3a52a5451ed6462c95c50a588513441bb3ed3bdc
Konrad Firlej <kofirlej@gmail.com>
2025-08-30 09:44:47 +0200

    fix: handle missing categories table on dashboard

---
+++ ViewModels/HomeDashboardVM.cs
@@ -0,0 +1,8 @@
+namespace LifeCare.ViewModels;
+
+public class HomeDashboardVM
+{
+    public int UsersCount { get; set; }
+    public int HabitsCount { get; set; }
+    public int CategoriesCount { get; set; }
+}
