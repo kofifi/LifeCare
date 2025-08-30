59b5e33deea81e26ccc0242cee40d967c92d7570
Konrad Firlej <kofirlej@gmail.com>
2025-08-30 08:39:03 +0200

    feat: link commits from news page

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using LifeCare.Models;
 
 namespace LifeCare.Controllers;
 
@@ -6,13 +10,29 @@ public class NewsController : Controller
 {
     public IActionResult Index()
     {
-        return View();
-    }
+        var psi = new ProcessStartInfo
+        {
+            FileName = "git",
+            Arguments = "log --pretty=format:%h:::%s",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            WorkingDirectory = Directory.GetCurrentDirectory()
+        };
 
-    public IActionResult Article(int id)
-    {
-        ViewData["ArticleId"] = id;
-        return View();
+        using var process = Process.Start(psi);
+        var output = process!.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        var commits = output
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Select(line =>
+            {
+                var parts = line.Split(":::", 2);
+                return new CommitInfo { Hash = parts[0], Message = parts.Length > 1 ? parts[1] : string.Empty };
+            })
+            .ToList();
+
+        return View(commits);
     }
 }
 
